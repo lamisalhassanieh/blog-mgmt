8a902de5ef4f84464d06c6c8125de5e84168e036
Lamis <lalhassanieh@borninteractive.com>
2025-03-18 16:30:56 +0200

    added env in next config

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   appIsrStatus: false,
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 export default nextConfig;
